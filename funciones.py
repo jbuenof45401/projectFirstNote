889a3b3d6f0f45b9d96d58d45b6a449199d68f0a
Juan Bueno <jbuenof45401@universidadean.edu.co>
2019-02-25 00:45:05 -0500

    rama de desarrollo primeras funciones

--- funciones.py
+++ funciones.py
@@ -1,21 +1,86 @@
 def calcular_precio_producto(coste_producto):
-    pass
+    '''
+    (num) -> float: valor del producto
+
+    calcular el coste del producto aumentando el 50% de su valor como comision
+
+    >>> calcular_precio_producto(5000)
+    7500.0
+
+    >>> calcular_precio_producto(1000)
+    1500.0
+
+    :param coste_producto: representa el coste del producto
+    :return: valor del producto
+    '''
+    return coste_producto + (coste_producto * 0.5)
 
 
 def calcular_precio_servicio(cantidad_horas):
-    pass
+    '''
+    (num) -> num: precio por horas trabajadas
+
+    >>> calcular_precio_servicio(5)
+    500000
+
+    >>> calcular_precio_servicio(4)
+    400000
+
+    :param cantidad_horas: horas de servicio prestado
+    :return: valor de las horas prestadas
+    '''
+
+    return cantidad_horas * 100000
 
 
 def calcular_precio_servicio_extras(cantidad_horas):
-    pass
+    '''
+    (num) -> float: recibe las horas extra trabajadas, devuelve el valor por las horas extra
+
+    >>> calcular_precio_servicio_extras(1)
+    125000.0
+    >>> calcular_precio_servicio_extras(2)
+    250000.0
+
+    :param cantidad_horas: cantidad de horas extra trabajadas
+    :return: valor de las horas extra
+    '''
+
+    precio_hora_normal = calcular_precio_servicio(cantidad_horas)
+
+    return precio_hora_normal + (precio_hora_normal*0.25)
 
 
 def calcular_costo_envio(kilometros):
-    pass
+    '''
+    (num) -> num: recibe kilometros y devuelve el valor por los kilometros recorridos
+
+    >>> calcular_costo_envio(2)
+    230
+
+    >>> calcular_costo_envio(3)
+    345
+
+    :param kilometros: cantidad de kilometros recorridos
+    :return: valor por los kilometros recorridos
+    '''
+
+    return  kilometros * 115
 
 def calcular_precio_producto_fuera(coste_producto,
                                    kilometros):
-    pass
+    '''
+    (num) -> num: devuelve el valor del producto y el recargo por kilometro recorrido
+
+
+    :param coste_producto: num: costo del producto para jose
+    :param kilometros: num: kilometros recorridos por jose
+    :return: float: precio del producto mas recargo de envio
+    '''
+
+    return calcular_precio_producto(coste_producto) + calcular_costo_envio(kilometros)
+
+
 
 
 def calcular_iva_producto(coste_producto, tasa):
